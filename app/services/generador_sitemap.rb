bdb1ae6ae44c686d6f395c28f91715528620ce2a
Lazarte, Hector <hlazarte@everfi.com>
2025-01-31 17:35:57 -0400

    Filtrar por email

--- app/services/generador_sitemap.rb
+++ app/services/generador_sitemap.rb
@@ -8,10 +8,13 @@ class GeneradorSitemap
     # Asegurarse de que el directorio /public/sitemaps existe
     ruta_sitemaps = Rails.root.join("public", "sitemaps")
     Dir.mkdir(ruta_sitemaps) unless Dir.exist?(ruta_sitemaps)
+    total = 0
     # Generar sitemaps por ciudades
     Ciudad.includes(:zonas, :comercios).find_each do |ciudad|
       next unless ciudad.comercios.count >= 100 # Solo para ciudades con 100 o más comercios
 
+      fecha_hoy = Date.today.strftime('%Y-%m-%d')
+      counter = 0
       nombre_archivo = "#{ciudad.ciudad.gsub(' ', '')}.xml"
       ruta_archivo = Rails.root.join("public", "sitemaps", nombre_archivo)
 
@@ -23,30 +26,38 @@ class GeneradorSitemap
           xml.url do
             xml.loc "#{BASE_URL}/Bolivia/#{ciudad.ciudad.gsub(' ', '-') }"
             xml.changefreq "weekly"
+            xml.lastmod fecha_hoy
           end
+          counter += 1
 
           # Nivel zonas
           ciudad.zonas.each do |zona|
             xml.url do
               xml.loc "#{BASE_URL}/Bolivia/#{ciudad.ciudad.gsub(' ', '-')}/#{zona.descripcion.gsub(' ', '-')}"
               xml.changefreq "weekly"
+              xml.lastmod fecha_hoy
             end
+            counter += 1
           end
 
           # Nivel comercios
-          ciudad.comercios.where(persona_natural: 'FALSE').each do |comercio|
+          ciudad.comercios.where(persona_natural: 'FALSE').where.not(email: nil).each do |comercio|
             xml.url do
               xml.loc "#{BASE_URL}/Bolivia/#{ciudad.ciudad.gsub(' ', '-')}/#{comercio.empresa.downcase.gsub(' ', '-')}"
               xml.changefreq "weekly"
+              xml.lastmod fecha_hoy
             end
+            counter += 1
           end
         end
       end
-
+      total += counter
+      puts "#{counter} Registro Generados"
       puts "Sitemap generado: #{ruta_archivo}"
     end
+  puts "#{total} TOTAL"
   end
 end
 
 # Ejecución del servicio
-GeneradorSitemap.generar
+# GeneradorSitemap.generar
