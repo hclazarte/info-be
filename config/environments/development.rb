5aa55042207235bba5d7ead4f95fcb3a4246cd6b
Lazarte, Hector <hlazarte@everfi.com>
2025-04-19 16:59:34 -0400

    base URL3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  Rails.application.config.base_url = 'https://dev.infomovil.com.bo'
+  config.base_url = 'https://dev.infomovil.com.bo'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
