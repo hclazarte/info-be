bf2d832c1bf8ef5d6312787c744e8508b1f165fc
Lazarte, Hector <hlazarte@everfi.com>
2025-05-01 10:50:55 -0400

    Inst  cache en REDIS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'], # define esta variable en tu entorno
+    namespace: 'infomovil_cache'
+  }
 end
