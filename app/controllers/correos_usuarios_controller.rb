8cec38fa0fb9846a3742f31764d253924324c687
Lazarte, Hector <hlazarte@everfi.com>
2025-05-22 19:21:29 -0400

    Arreglar controlador

--- app/controllers/correos_usuarios_controller.rb
+++ app/controllers/correos_usuarios_controller.rb
@@ -1,37 +1,33 @@
 class CorreosUsuariosController < ApplicationController
   include RecaptchaVerifiable
 
-  protect_from_forgery with: :null_session
-
   def create
-    comercio = Comercio.find_by(id: params[:comercio_id])
+    comercio_id = params.dig(:correos_usuario, :comercio_id)
+    comercio = Comercio.find_by(id: comercio_id)
 
-    if comercio.nil? || comercio.email.blank? || comercio.email_verificado.blank?
+    if comercio.nil? || comercio.email_verificado.blank?
       return render json: { error: 'Comercio sin correo verificado' }, status: :unprocessable_entity
     end
 
-    correo = CorreoUsuario.new(
-      remitente: params[:remitente],
-      destinatario: comercio.email, # ahora se obtiene internamente
-      asunto: params[:asunto],
-      cuerpo: params[:cuerpo],
-      nombre: params[:nombre],
-      comercio_id: comercio.id,
-      fecha: Time.current,
-      estado: :pendiente
-    )
+    correo = CorreoUsuario.new(correo_params.merge(destinatario: comercio.email_verificado))
 
     if correo.save
       begin
         EmailProtegido.deliver_later(CorreoUsuarioMailer, :enviar_mensaje_usuario, correo)
         puts "Enviando correo a #{correo.destinatario}"
       rescue EmailProtegido::EmailBloqueadoError => e
-        puts e.message         # “Envío bloqueado: …”
+        puts e.message
       end
       render json: { ok: true, id: correo.id }, status: :created
     else
+      puts correo.errors.full_messages
       render json: { errors: correo.errors.full_messages }, status: :unprocessable_entity
     end
   end
-end
 
+  private
+
+  def correo_params
+    params.require(:correos_usuario).permit(:nombre, :remitente, :asunto, :cuerpo, :comercio_id)
+  end
+end
