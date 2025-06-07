87fd12f8b4c46843e889a69b185c00f19623471f
Lazarte, Hector <hlazarte@everfi.com>
2025-06-07 16:10:44 -0400

    Arreglar enums

--- app/models/whatsapp_mensaje.rb
+++ app/models/whatsapp_mensaje.rb
@@ -1,9 +1,10 @@
 class WhatsappMensaje < ApplicationRecord
   belongs_to :whatsapp_chat
 
-  enum remitente: { usuario: 0, comercio: 1, plataforma: 2 }
-  enum destinatario: { usuario: 0, comercio: 1 }
+  enum remitente: { usuario: 0, comercio: 1, plataforma: 2 }, _prefix: :rem
+  enum destinatario: { usuario: 0, comercio: 1 }, _prefix: :des
 
   validates :cuerpo, presence: true
   validates :remitente, presence: true
+  validates :destinatario, presence: true
 end
