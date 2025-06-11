c2ad911927b57b067a90400561ff2ddecf7e1c38
Lazarte, Hector <hlazarte@everfi.com>
2025-06-11 11:05:51 -0400

    Arreglar el modelo

--- app/models/usuario_whatsapp.rb
+++ app/models/usuario_whatsapp.rb
@@ -3,4 +3,9 @@ class UsuariosWhatsapp < ApplicationRecord
 
   validates :celular, presence: true, uniqueness: true
   validates :nombre, presence: true
+
+
+  def whatsapp_autorizado?
+    whatsapp_fecha_autorizado.present? && whatsapp_fecha_autorizado > 24.hours.ago
+  end
 end
--- app/models/whatsapp_chat.rb
+++ app/models/whatsapp_chat.rb
@@ -3,4 +3,12 @@ class WhatsappChat < ApplicationRecord
   belongs_to :comercio
 
   validates :mensaje, presence: true
+
+   def texto_para_envio
+    <<~MSG
+    Usuario: #{usuarios_whatsapp.nombre}
+    Celular: #{usuarios_whatsapp.celular}
+    Mensaje: #{mensaje}
+    MSG
+  end
 end
